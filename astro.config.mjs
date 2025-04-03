80bc74b954848e772a4587abc6d446dc2f7b5fa4
Chris Pan <cpanorios@gmail.com>
2025-04-03 19:25:21 +0300

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,6 @@ export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [react(), tailwind(), sitemap()]
+  integrations: [react(), tailwind(), sitemap()],
+  site: 'chrisp2797.github.io'
 });
