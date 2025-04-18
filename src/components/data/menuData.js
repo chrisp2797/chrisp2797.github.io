eced5d047883a4f4a031aef3979d5a0d28eb16ca
Chris Pan <cpanorios@gmail.com>
2025-04-18 20:37:19 +0300

    changes

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,7 +1,7 @@
 export const mainMenu = [
      {link:"#", content:"Home", icon:"lucide:home"},
      {link:"#about", content:"About", icon:"octicon:person-24"},
-     {link:"#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
+     {link:"#portfolio", content:"Portfolio", icon:"carbon:portfolio"},
 
 ]
 export const menuBlog = [
